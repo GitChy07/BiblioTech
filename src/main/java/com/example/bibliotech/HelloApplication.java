52d113a1b5a20615ebedc21d2ca8d8f1a75d6891
eliggi <elias.dueggeli@fhnw.ch>
2025-05-05 11:07:57 +0200

    first commit

--- src/main/java/com/example/bibliotech/HelloApplication.java
+++ src/main/java/com/example/bibliotech/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    //naim isch sippi
 
     public static void main(String[] args) {
         launch();
